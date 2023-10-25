a16244971e9add97833e7cce45aba0ea7b302829
siddhiGvc <siddhi@gvc.in>
2023-10-25 17:41:33 +0530

    inital value and colors added

--- client/src/components/simulator/Simulator.jsx
+++ client/src/components/simulator/Simulator.jsx
@@ -4,6 +4,41 @@ import { TextField } from "@mui/material";
 import { useState } from "react";
 
 export default function Simulator(){
+   const [machine1,setMachine1]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine2,setMachine2]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine3,setMachine3]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine4,setMachine4]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine5,setMachine5]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine6,setMachine6]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine7,setMachine7]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine8,setMachine8]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine9,setMachine9]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine10,setMachine10]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine11,setMachine11]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine12,setMachine12]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine13,setMachine13]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine14,setMachine14]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine15,setMachine15]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine16,setMachine16]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine17,setMachine17]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine18,setMachine18]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine19,setMachine19]=useState({machine_number:"", bgColor:'black',value:0})
+   const [machine20,setMachine20]=useState({machine_number:"", bgColor:'black',value:0})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -13,64 +48,64 @@ export default function Simulator(){
 <div className={styles.container}>
        
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine1.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine1.machine_number}${machine1.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine2.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine2.machine_number}${machine2.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine3.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine3.machine_number}${machine3.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine4.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine4.machine_number}${machine4.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine5.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine5.machine_number}${machine5.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine6.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine6.machine_number}${machine6.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine7.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine7.machine_number}${machine7.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine8.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine8.machine_number}${machine8.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine9.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine9.machine_number}${machine9.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine10.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine10.machine_number}${machine10.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine11.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine11.machine_number}${machine11.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine12.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine12.machine_number}${machine12.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine13.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine13.machine_number}${machine13.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine14.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine14.machine_number}${machine14.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine15.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine15.machine_number}${machine15.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine16.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine16.machine_number}${machine16.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine17.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine17.machine_number}${machine17.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine18.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine18.machine_number}${machine18.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine19.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine19.machine_number}${machine19.value}`}</Button>
         </div>
         <div className={styles.buttonBox}>
-        <Button variant="contained" sx={{backgroundColor:'#0094CE',position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>QUERY</Button>
+        <Button variant="contained" sx={{backgroundColor:`${machine20.bgColor}`,position:"static",width:"100%",margin:"auto",height:"40px",fontSize:"100%",fontWeight:"400"}}>{`${machine20.machine_number}${machine20.value}`}</Button>
         </div>
       
       
@@ -84,151 +119,145 @@ export default function Simulator(){
         <div className={styles.rowContainer}>
         <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 1"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 2"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 3"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 4"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 5"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 6"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 7"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 8"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 9"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 10"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 11"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 12"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 13"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 14"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 15"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 16"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 17"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 18"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 19"
           multiline
           maxRows={4}
           variant="standard"
         />
            <TextField
           id="standard-multiline-flexible"
-          label="Max Temp Primary"
+          label="Machine 20"
           multiline
           maxRows={4}
           variant="standard"
         />
-           <TextField
-          id="standard-multiline-flexible"
-          label="Max Temp Primary"
-          multiline
-          maxRows={4}
-          variant="standard"
-        />
+         
 
         </div>
       
